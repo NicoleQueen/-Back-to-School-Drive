3708b3c15a76fafa77ae167bc83a00e904fe0bc1
Gabe571 <gabriel.castro1028@gmail.com>
2020-07-28 15:45:06 -0700

    Supply List

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,68 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+aa1 = Supply.create(supply: “Pencils”, amount: 50, school_id: 1)
+aa2 = Supply.create(supply: “Notebooks”, amount: 30, school_id: 1)
+aa3 = Supply.create(supply: “Binders”, amount: 10, school_id: 1)
+aa4 = Supply.create(supply: “Markers”, amount: 20, school_id: 1)
+aa5 = Supply.create(supply: “Laptops”, amount: 20, school_id: 1)
+
+
+mu1 = Supply.create(supply: “Pencils”, amount: 50, school_id: 2)
+mu2 = Supply.create(supply: “Notebooks”, amount: 30, school_id: 2)
+mu3 = Supply.create(supply: “Binders”, amount: 10, school_id: 2)
+mu4 = Supply.create(supply: “Markers”, amount: 20, school_id: 2)
+mu5 = Supply.create(supply: “Tape”, amount: 20, school_id: 2)
+
+wv1 = Supply.create(supply: “Pencils”, amount: 50, school_id: 3)
+wv2 = Supply.create(supply: “Notebooks”, amount: 30, school_id: 3)
+wv3 = Supply.create(supply: “Binders”, amount: 10, school_id: 3)
+wv4 = Supply.create(supply: “Markers”, amount: 20, school_id: 3)
+wv5 = Supply.create(supply: “Hand Sanatizer Bottles”, amount: 20, school_id: 3)
+
+tt1 = Supply.create(supply: “Pencils”, amount: 50, school_id: 4)
+tt2 = Supply.create(supply: “Notebooks”, amount: 30, school_id: 4)
+tt3 = Supply.create(supply: “Binders”, amount: 10, school_id: 4)
+tt4 = Supply.create(supply: “Markers”, amount: 20, school_id: 4)
+tt5 = Supply.create(supply: “Clorox Wipes”, amount: 20)
+
+gs1 = Supply.create(supply: “Pencils”, amount: 50, school_id: 5)
+gs2 = Supply.create(supply: “Notebooks”, amount: 30, school_id: 5)
+gs3 = Supply.create(supply: “Crayons”, amount: 10, school_id: 5)
+gs4 = Supply.create(supply: “Folders”, amount: 20, school_id: 5)
+gs5 = Supply.create(supply: “Flash Cards”, amount: 20, school_id: 5)
+
+cd1 = Supply.create(supply: “Pencils”, amount: 50, school_id: 6)
+cd2 = Supply.create(supply: “Notebooks”, amount: 30, school_id: 6)
+cd3 = Supply.create(supply: “Binders”, amount: 10, school_id: 6)
+cd4 = Supply.create(supply: “Markers”, amount: 20, school_id: 6)
+cd5 = Supply.create(supply: “Erasers”, amount: 20, school_id: 6)
+
+we1 = Supply.create(supply: “Pencils”, amount: 50, school_id: 7)
+we2 = Supply.create(supply: “Notebooks”, amount: 30, school_id: 7)
+we3 = Supply.create(supply: “Binders”, amount: 10, school_id: 7)
+we4 = Supply.create(supply: “Markers”, amount: 20, school_id: 7)
+we5 = Supply.create(supply: “Manual Pencil Sharpeners”, amount: 20, school_id: 7)
+
+cc1 = Supply.create(supply: “Pencils”, amount: 50, school_id: 8)
+cc2 = Supply.create(supply: “Notebooks”, amount: 30, school_id: 8)
+cc3 = Supply.create(supply: “Binders”, amount: 10, school_id: 8)
+cc4 = Supply.create(supply: “Markers”, amount: 20, school_id: 8)
+cc5 = Supply.create(supply: “Pens”, amount: 20, school_id: 8)
+
+pp1 = Supply.create(supply: “Backpacks”, amount: 50, school_id: 9)
+pp2 = Supply.create(supply: “Notebooks”, amount: 30, school_id: 9)
+pp3 = Supply.create(supply: “Binders”, amount: 10, school_id: 9)
+pp4 = Supply.create(supply: “Markers”, amount: 20, school_id: 9)
+pp5 = Supply.create(supply: “Clipboards”, amount: 20, school_id: 9)
+
+ew1 = Supply.create(supply: “Pencils”, amount: 50, school_id: 10)
+ew2 = Supply.create(supply: “Notebooks”, amount: 30, school_id: 10)
+ew3 = Supply.create(supply: “Binders”, amount: 10, school_id: 10)
+ew4 = Supply.create(supply: “Markers”, amount: 20, school_id: 10)
+ew5 = Supply.create(supply: “Staplers”, amount: 20, school_id: 10)
+
+
+
