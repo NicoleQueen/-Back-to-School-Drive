4579c0b3200b47083f6a79de82a134fca72392d8
NicoleQiu99 <nicolepeng99@gmail.com>
2020-07-28 11:36:37 -0700

    add school data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+acceleratedAcademy = School.create(name:"Accelerated Academy", district:"Seattle", description:"High school")
+meadowsUniversity= School.create(name:"Meadows University", district:"San Francisco", description:"University")
+westviewKindergarten= School.create(name:"Westview Kindergarten", district:"San Francisco", description:"Preschool")
+tulipTree= School.create(name:"Tulip Tree", district:"San Francisco", description:"High school")
+greenSprout= School.create(name:"Green Sprout", district:"Seattle", description:"High school")
+countryDaySchool= School.create(name:"Country Day School", district:"Seattle", description:"Day School")
+wheePreschool= School.create(name:"Whee Preschool", district:"San Francisco", description:"Preschool")
+creatingChampions= School.create(name:"Creating Champions", district:"Seattle", description:"Academy")
+prioritizePrimary= School.create(name:"Prioritize Primary", district:"Seattle", description:"Academy")
+eastwoodHigh= School.create(name:"Eastwood High", district:"Seattle", description:"English school")
