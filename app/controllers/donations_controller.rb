2586914f27d9d0c16ddd2b4cccdcbf67f5194e63
cortezd334 <cortezd334@gmail.com>
2020-07-30 22:03:19 -0700

    donations post still needs work

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -12,6 +12,13 @@ class DonationsController < ApplicationController
     end
     
     def create
+        # byebug
+        user = User.find_or_create_by(name: params[:donation][:name])
+
+        supply = Supply.find_or_create_by(supply: params[:donation][:supply], amount: params[:donation][:amount], school_id: params[:donation][:school_id])
+        
+        @donation = Donation.create(amount: params[:donation][:amount], user_id: user.id, supply_id: supply.id)
+        render json: @donation
     end
 end
 
